6dc95018517e0eac8b1db472c1f8b971535ed9f9
renatoguara2020 <renatoguara2020@gmail.com>
2023-03-09 09:44:17 -0300

    ok

--- src/db.js
+++ src/db.js
@@ -1,12 +1,7 @@
 import { Sequelize } from 'sequelize';
-import dotenv from 'dotenv/config.js';
 
-const dbName = process.env.DB_NAME;
-const dbUser = process.env.DB_USER;
-const dbHost = process.env.DB_HOST;
-const dbPassword = process.env.DB_PASSWORD;
 
-const sequelize = new Sequelize(dbName, dbUser, dbPassword, {
+const sequelize = new Sequelize('sequelizenode', 'root', '', {
     dialect: 'mysql',
     host: dbHost
 })
