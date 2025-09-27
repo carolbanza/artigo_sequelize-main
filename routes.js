93c509826f236f81ac9b29c8dcdc83a8ad199ba3
renatoguara2020 <renatoguara2020@gmail.com>
2025-09-27 15:25:12 -0300

    ok

--- routes.js
+++ routes.js
@@ -3,10 +3,11 @@ import clients from './src/controllers/clients.js';
 
 const routes = express.Router();
 
-routes.get('/clients',clients.findAll);
+routes.get('/clientsTodos',clients.findAll);
 routes.get('/clients/:id',clients.findClient);
 routes.put('/clients/:id',clients.updateClient);
 routes.delete('/clients/:id',clients.deleteClient);
-routes.post('/clients', clients.addClient);
+routes.post('/clientsAdd', clients.addClient);
+
 
 export { routes as default };
--- server.js
+++ server.js
@@ -1,15 +1,58 @@
 import express from 'express';
 import routes from './routes.js';
 import db from './src/db.js';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import path from 'path';
+import bodyParser from "body-parser";
+import clientes from "./src/controllers/clients.js"
+import exphbs from "express-handlebars"
+
 
 const app = express();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+app.engine('handlebars', exphbs.engine({defaultLayout: 'main'}))
+//app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+
+app.set('views', path.resolve(__dirname, 'src', 'views')); 
+app.set('view engine', 'handlebars');
+app.use(express.static(path.resolve(__dirname, 'public')));
+
 app.use(express.json());
 app.use(routes);
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+//app.use(express.urlencoded({extended: true}))
+
+
+app.get('/', (req, res) =>{
+
+  res.render("index")
+})
+// Rota para receber e processar os dados do formulário
+app.post('/clientesAdd', (req, res) => {
+
+  let nome = req.body.nome
+  let email = req.body.email
+
+
+  
+  console.log(`Meu nome é ${nome} e meu e-mail é: ${email}`)
+
+  res.render("mostrar")
+});
+
 
 db.sync( () => 
-    console.log(`Banco de dados conectado: ${process.env.DB_NAME}`)
-);
+    console.log(`Banco de dados conectado: ${bd}`)
+).catch( (e) => {
+
+  console.log(`Algo deu errado ${e}`)
+  
+});
 
 app.listen(3000, () => 
     console.log('Servidor iniciado na porta 3000')
